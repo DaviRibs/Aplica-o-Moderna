f0ec3bbd0aa92b05152f09f01481a07e965c7fad
unknown <davirb21@gmail.com>
2021-08-24 20:46:40 -0300

    assertives

--- cypress/integration/examples/asserts.spec.js
+++ cypress/integration/examples/asserts.spec.js
@@ -1,6 +1,25 @@
 /// <reference types="cypress" />
 
+const { escapeSelector } = require("cypress/types/jquery");
+
 it('Equality',  () => {
 const a = 1;
+
+expect(a).equal(1);
+expect(a, 'Deveria ser 1').equal(1);
+expect(a).to.be.equal(1);
+expect('a').not.to.be.equal('b')
+
+})
+
+it('Truthy', () =>{
+const a = true;
+const b= null;
+const c;
+
+expect(a).to.be.true;
+expect(true).to.be.true;
+escapeSelector(b).to.be.null;
+
 
 })
